98e1797f1ab957e87e26d45380512f47e2898424
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-05-30 16:58:50 -0400

    created home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,18 @@ import "./home.scss"
 
 export default function Home() {
   return (
-    <div className="home" id="home"> Home</div>
+    <div className="home" id="home">
+        <div className="left">
+            <div className="imgContainer"><img src="assets/headshot.png" alt="Hi! It's me, Reeth!"/></div>
+            
+        </div>
+        <div className="right">
+            <div className="wrapper">
+                <h2>Hello and welcome! My name is </h2>
+                <h1>Reeth Dasgupta</h1>
+                <h3>Fullstack Developer<span></span></h3>
+            </div>
+        </div>
+    </div>
   )
 }
