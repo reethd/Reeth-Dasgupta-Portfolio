9aa29790a798f63cd446b3e4f31330da1296c3d0
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-06-05 18:57:44 -0400

    added portfolio data file for dynamic portfolio entries

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,48 +1,93 @@
-import "./portfolio.scss"
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
+import "./portfolio.scss";
+import {
+  weatherDashboard,
+  socialNetworkApi,
+  everybodyEats,
+  readmeGenerator,
+  employeeTracker,
+  digitalDiyBulletinBoard,
+  freeSchool,
+} from "../../portfolioData";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("weather dashboard");
+
+  const list = [
+    {
+      id: "weather dashboard",
+      title: "Weather Dashboard",
+    },
+    {
+      id: "social network api",
+      title: "Social Network API",
+    },
+    {
+      id: "everybody eats",
+      title: "Everybody Eats",
+    },
+    {
+      id: "readme generator",
+      title: "Readme Generator",
+    },
+    {
+      id: "employee tracker",
+      title: "Employee Tracker",
+    },
+    {
+      id: "digital diy bulletin board",
+      title: "Digital DIY Bulletin Board",
+    },
+    {
+      id: "free school",
+      title: "Free School",
+    },
+  ];
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Weather Dashboard</li>
-        <li>Social Network API</li>
-        <li>Everybody Eats</li>
-        <li>Readme Generator</li>
-        <li>Employee Tracker</li>
-        <li>Digital DIY Bulletin Board</li>
-        <li>Free School</li>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
-          <img src="../assets/weatherDash.png"/>
+          <img src="../assets/weatherDash.png" />
           <h3>Weather Dashboard</h3>
         </div>
         <div className="item">
-          <img src="../assets/socialNetwork.png"/>
+          <img src="../assets/socialNetwork.png" />
           <h3>Social Network API</h3>
         </div>
         <div className="item">
-          <img src="../assets/everybodyEats.png"/>
+          <img src="../assets/everybodyEats.png" />
           <h3>Everybody Eats</h3>
         </div>
         <div className="item">
-          <img src="../assets/readmeGenerator.png"/>
+          <img src="../assets/readmeGenerator.png" />
           <h3>Readme Generator</h3>
         </div>
         <div className="item">
-          <img src="../assets/employeeTracker.png"/>
+          <img src="../assets/employeeTracker.png" />
           <h3>Employee Tracker</h3>
         </div>
         <div className="item">
-          <img src="../assets/ddbb.png"/>
+          <img src="../assets/ddbb.png" />
           <h3>Digital DIY Bulletin Board</h3>
         </div>
         <div className="item">
-          <img src="../assets/freeschool.png"/>
+          <img src="../assets/freeschool.png" />
           <h3>Free School</h3>
         </div>
       </div>
     </div>
-  )
+  );
 }
