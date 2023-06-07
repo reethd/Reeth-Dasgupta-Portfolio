ba30740c69d7a9e29290b975ddfc0edad2ab9462
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-06-07 17:33:15 -0400

    added header links and resume

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -16,7 +16,7 @@ export default function Portfolio() {
   };
 
   return (
-    <div className="portfolio">
+    <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <div
         className="slider"
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -2,6 +2,18 @@ import "./resume.scss"
 
 export default function Resume() {
   return (
-    <div className="resume" id="resume">Resume</div>
+    <div className="resume" id="resume">
+      <h1>Resume</h1>
+      <div className="container">
+      <iframe
+        title="resume"
+        className="resWindow"
+        src="../assets/resume.pdf"
+        width="100%"
+        height="450px"
+      >
+      </iframe>
+    </div>
+      </div>
   )
 }
