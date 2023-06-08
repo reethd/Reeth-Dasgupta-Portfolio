f2c42c6b7fe4a64f4080317954ff852f9dfe3d9a
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-06-08 17:06:11 -0400

    added contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import "./contact.scss"
 
 export default function Contact() {
+
+const [message, setMessage] = useState(false)
+  
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setMessage(true);
+}
+
   return (
-    <div className="contact" id="contact">Contact</div>
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/contactImg.png"/>
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}> 
+          <input type="email" placeholder="Email"/>
+            <textarea placeholder="Message"></textarea>
+            <button type="submit">Send</button>
+            {message && <span>Thanks for reaching out!</span>}
+        </form>
+      </div>
+    </div>
   )
 }
