47ce69ab32036bc42fa004b3ce8cf65b53fd4f03
Reeth Dasgupta <115037176+reethd@users.noreply.github.com>
2023-06-10 15:58:48 -0400

    added contact form functionality with formspark

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,45 @@
-import { useState } from "react";
+import { useState, } from "react";
 import "./contact.scss"
+import axios from "axios"
+
+
 
 export default function Contact() {
 
+const formId ="p03Xdldx"
+const formSparkUrl = `https://submit-form.com/${formId}`
+
+
+
 const [message, setMessage] = useState(false)
+const [formState, setFormState] = useState({
+  email: '',
+  name: '',
+  message: '',
+});
   
-const handleSubmit = (e) => {
+const handleSubmit = async (e) => {
   e.preventDefault();
-  setMessage(true);
+  setMessage(true); 
+  await postSubmission();
+}
+
+const postSubmission = async () => {
+  const payload = {
+    ...formState
+  };
+
+  try {
+    await axios.post(formSparkUrl, payload);
+    await setFormState({
+      email: '',
+      name: '',
+      message: '',
+    });
+  }
+  catch(error) {
+    console.log(error);
+  }
 }
 
   return (
@@ -18,8 +50,9 @@ const handleSubmit = (e) => {
       <div className="right">
         <h2>Contact</h2>
         <form onSubmit={handleSubmit}> 
-          <input type="email" placeholder="Email"/>
-            <textarea placeholder="Message"></textarea>
+          <input type="email" placeholder="Email" value={formState.email} onChange={(e)=>setFormState({...formState, email: e.target.value})}/>
+          <input type="text" placeholder="Name/Company" value={formState.name} onChange={(e)=>setFormState({...formState, name: e.target.value})}/>
+            <textarea placeholder="Message" value={formState.message} onChange={(e)=>setFormState({...formState, message: e.target.value})}></textarea>
             <button type="submit">Send</button>
             {message && <span>Thanks for reaching out!</span>}
         </form>
